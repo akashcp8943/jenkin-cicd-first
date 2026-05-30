3ab314f371a7911c9c48cc1605f48ab70d20e197
Akash cp <115542840+akashcp8943@users.noreply.github.com>
2026-05-30 14:17:37 +0530

    Fix project name and welcome message in index.js

--- project-2-pipeline-jenkinsfile/index.js
+++ project-2-pipeline-jenkinsfile/index.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 3002;
 app.get("/", (req, res) => {
   res.status(200).json({
     status: "ok",
-    project: "project-2-pipeline-jenkinsfiles",
-    message: "Welcome to Project 2 APIs",
+    project: "project-2-pipeline-jenkinsfile",
+    message: "Welcome to Project 2 API",
   });
 });
 
